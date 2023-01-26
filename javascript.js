71b370a0182064a089a5771cb7a4d731c2ba14a4
Jalan Stanford <jalan.stanford@yahoo.co.nz>
2023-01-26 16:39:21 +1300

    Add Boolean output based on randomNumGen

---
+++ javascript.js
@@ -0,0 +1,37 @@
+function getComputerChoice() {
+    const Rock = 1;
+    const Paper = 2;
+    const Scissor = 3;
+    Math.random()
+    console.log(Math.random());
+   
+   
+    let test = 0; 
+    test = Math.random();
+
+    if (test < 0.5) {
+        console.log("true");
+    } else {
+     console.log("false")
+    }
+   
+    
+    if (Math.random == 1) {
+        console.log()
+    }
+
+    if (Math.random <= 0.33) {
+        console.log(Rock)
+    }
+
+    getRandomInt(3)
+    console.log(getRandomInt(3))
+}
+
+
+
+
+
+
+
+getComputerChoice()
